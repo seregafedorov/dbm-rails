b888648cbfdcf763666bbc30f9792cb58119fca2
bob <mike.bobrov@gmail.com>
2014-06-25 21:47:24 +0400

    поставил дефолтовую локаль английскую

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Dbm
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:en, :ru]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ru
 
     config.assets.precompile += %w( application.js index.css )
 
