acb45a0a11c695e2cce60620838cb47a280202f0
bob <mike.bobrov@gmail.com>
2014-06-26 22:51:33 +0400

    попытка засунуть pid в другую директорию

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -1,6 +1,6 @@
 deploy_to  = '/var/www/apps/dbm_staging'
 rails_root = "#{deploy_to}/current"
-pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
+pid_file   = "#{deploy_to}/shared/tmp/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/unicorn.sock"
 log_file   = "#{rails_root}/log/unicorn.log"
 err_log    = "#{rails_root}/log/unicorn_error.log"
