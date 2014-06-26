43c7c2313e987812e3b5728757051121dba7764f
bob <mike.bobrov@gmail.com>
2014-06-26 17:12:08 +0400

    вывел поля для проекта

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show]
 
   def index
-    @menu_projects = Project.all
+    @projects = Project.all
   end
 
   def show
